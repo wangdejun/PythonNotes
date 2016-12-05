a18b22fe373e9fef7b073359977622bea324f7be
wangdejun <1074201799@qq.com>
2016-12-06 00:00:03 +0800

    thread

---
+++ memory_manage1.py
@@ -0,0 +1,63 @@
+a = 1
+b = 1
+c = 1
+print(id(a))
+print(id(b))
+print(id(c))
+print(hex(id(a)))
+print(hex(id(b)))
+print(hex(id(c)))
+
+print(a is b)
+print(b is c)
+
+w = "wangdejun is a good programmer"
+m = "wangdejun is a good programmer"
+
+print(a is b)
+
+#getrefcount()
+from sys import getrefcount
+# a=0
+# print("1,after the defination:",getrefcount(a))
+a = [1,2,3]
+print("2,after the =: ",getrefcount(a))
+b = a
+print("3,after another = ",getrefcount(b))
+
+class from_obj(object):
+    def __init__(self,to_obj):
+        self.to_obj = to_obj
+
+b = [1,2,3]
+a = from_obj(b)
+print(id(a.to_obj))
+print(id(b))
+
+a = [1,2,3]
+print(getrefcount(a))
+b = [a,a]
+print("b=",b)
+print(getrefcount(a))
+
+
+#getrefcount()
+from sys import getrefcount
+
+
+# a=0
+print("1,after the defination:",getrefcount(a))
+a = [1,2,3]
+print("2,after the =: ",getrefcount(a))
+b = a
+print("3,after another = ",getrefcount(b))
+
+
+
+
+x = [1, 2, 3]
+y = [x, dict(key1=x)]
+z = [y, (x, y)]
+
+import objgraph
+objgraph.show_refs([z], filename='ref_topo.png')
--- thread.py
+++ thread.py
@@ -1,23 +1,62 @@
+# #!/usr/bin/python
+# # -*- coding: UTF-8 -*-
+
+# import thread
+# import time
+
+# # 为线程定义一个函数
+# def print_time( threadName, delay):
+#    count = 0
+#    while count < 5:
+#       time.sleep(delay)
+#       count += 1
+#       print "%s: %s" % ( threadName, time.ctime(time.time()) )
+
+# # 创建两个线程
+# try:
+#    thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+#    thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+# except:
+#    print "Error: unable to start thread"
+
+# while 1:
+#    pass
+
+
+
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import thread
+import threading
 import time
 
-# 为线程定义一个函数
-def print_time( threadName, delay):
-   count = 0
-   while count < 5:
-      time.sleep(delay)
-      count += 1
-      print "%s: %s" % ( threadName, time.ctime(time.time()) )
+exitFlag = 0
 
-# 创建两个线程
-try:
-   thread.start_new_thread( print_time, ("Thread-1", 2, ) )
-   thread.start_new_thread( print_time, ("Thread-2", 4, ) )
-except:
-   print "Error: unable to start thread"
+class myThread (threading.Thread):   #继承父类threading.Thread
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
+        print "Starting " + self.name
+        print_time(self.name, self.counter, 5)
+        print "Exiting " + self.name
 
-while 1:
-   pass
+def print_time(threadName, delay, counter):
+    while counter:
+        if exitFlag:
+            thread.exit()
+        time.sleep(delay)
+        print "%s: %s" % (threadName, time.ctime(time.time()))
+        counter -= 1
+
+# 创建新线程
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+# 开启线程
+thread1.start()
+thread2.start()
+
+print "Exiting Main Thread"
