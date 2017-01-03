519de794bef02588b66b8cf08c7ae7a3dd2cd957
wangdejun <1074201799@qq.com>
2017-01-03 23:52:23 +0800

    add copy()

--- copy.py
+++ copy.py
@@ -1,13 +1,28 @@
+# a = ["wang","de","jun"]
+# b = a
+# print a
+# print b
+# a[0]="li"
+# print a
+# print b
+
+# # output:
+# # ['wang', 'de', 'jun']
+# # ['wang', 'de', 'jun']
+# # ['li', 'de', 'jun']
+# # ['li', 'de', 'jun']
+
+
 a = ["wang","de","jun"]
 b = a
+c = list(a)
+d = a[:]
 print a
 print b
+print c
+print d
 a[0]="li"
 print a
 print b
-
-# output:
-# ['wang', 'de', 'jun']
-# ['wang', 'de', 'jun']
-# ['li', 'de', 'jun']
-# ['li', 'de', 'jun']
+print c
+print d
