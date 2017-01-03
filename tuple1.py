821c4bdaa2bcf4dd5b6b24ec1141e03aa8367d63
wangdejun <1074201799@qq.com>
2017-01-04 00:05:33 +0800

    add the tuple.py

---
+++ tuple1.py
@@ -0,0 +1,8 @@
+name = "wangdejun","de","jun"
+print name
+
+name1 = "wang"
+name2 = "dejun"
+print "before:name1=>%s name2=>%s"name1,name2
+name2,name1 = "wang","dejun"
+print "after:name1=>%s name2=>%s"name1,name2
