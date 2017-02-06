fd00b32e57d7bcfe63597729defc8c08af072c86
wangdejun <1074201799@qq.com>
2017-02-06 08:33:57 +0800

    declare a class and use its property function

--- first.py
+++ first.py
@@ -1,7 +1,16 @@
+#declare a class and definate a function in the class
 class SampleMore(object):
+    #transport the value self and a:
     def __call__(self,a):
-        return a+5;
+        # return the sum: return a tuple
+        return (a+5,a+6)
+
 
 add = SampleMore()
+
 print(add(2))
-map(add,[2,4,5])
+print map(add,[2,4,5])
+
+new_values = SampleMore()
+value1,value2 = new_values(100)
+print value1,value2
