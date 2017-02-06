abe0b4ae3df491884f8ab1c910d67b18354a5704
wangdejun <1074201799@qq.com>
2017-02-06 08:27:29 +0800

    comments

--- file_open_read_write.py
+++ file_open_read_write.py
@@ -1,13 +1,19 @@
+#open a file and assign the handler to variable file,mode write
 f = open('./file/data.txt','w')
+#write the world Hello and changed to nextline
 f.write('Hello\n')
 f.write('World\n')
 f.write('wangdejun\n')
 f.write('continue write some messages')
 f.close()
+#close the handler
 m = open('./file_0/data.txt','w')
 m.write('World\n')
 m.close()
 
 w = open('./file/data.txt')
+#assign the value to text
 text = w.read()
 print text
+#close the file
+w.close()
