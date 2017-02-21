a57f005c38fa5622d5cf75a12f463bf0862bd797
wangdejun <1074201799@qq.com>
2017-02-21 09:11:29 +0800

    comment on something

--- decorator.py
+++ decorator.py
@@ -1,23 +1,35 @@
+#decorator args参数装饰器
+
+
 def log(f):
     def wrapper(*args,**kw):
+        # 包装函数
+        # 打印传进来的函数name
         print "call %s():" % f.__name__
         print "i am going to print the name of the value! %s():" % f.__name__
+        #返回传进来的函数，可以发现装饰器在返回函数之前做了很多『装饰性』工作
         return f(*args,**kw)
+        # 返回包装函数
     return wrapper
 
-@log
+
+@log#使用装饰器
 def now():
     print "2016-12-1"
 
-
+#执行函数
 now()
+
+#定义一个新的装饰器，
+# 功能是在调用某函数之前打印函数名字，并且告知即将调用函数的名字
 
 def logFunctionName(thename):
 	def theNameWrapper(*args,**kw):
-		print "call %s():" %thename.__name__
+		print "call %s():" % thename.__name__
 		print "I am going to print the name of the value! %s():" % thename.__name__
-		return thename(*args,**kw)
+		return thename(*args, **kw)
 	return theNameWrapper
+
 
 @logFunctionName
 def hello():
