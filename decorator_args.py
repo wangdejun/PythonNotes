fb84fa9a5f3935cb5509ac83f4054d9ee5e13fc8
wangdejun <1074201799@qq.com>
2017-02-21 08:41:47 +0800

    decorator_args and decorator_class

--- decorator_args.py
+++ decorator_args.py
@@ -1,30 +1,7 @@
 # #a new wrapper layer 一个新的包装层
 
-
-# def pre_str(pre=""):
-#     def decorator(F):
-#         def new_F(a,b):
-#             print(pre + "input",a,b)
-#             return F(a,b)
-#         return new_F
-#     return decorator
-
-
-# @pre_str("^_^")
-# def square_sum(a,b):
-#     return a**2 + b**2
-
-# @pre_str("wangdejun")
-# def square_diff(a,b):
-#     return a**2 - b**2
-
-# print(square_sum(3,4))
-# print(square_diff(3,4))
-
-# new_square_sum  = pre_str("XXXXX ")(square_sum(3,4))
-# print new_square_sum
-
 def pre_str(pre=""):
+    #在函数内部define一个新的包装函数
     def decorator(F):
         def new_F(a,b):
             print pre+"input" +", "+ str(a)+", "+ str(b)
--- decorator_class.py
+++ decorator_class.py
@@ -1,15 +1,22 @@
+#装饰类
+
 def decorator(aClass):
     class newClass:
+        #类初始化
         def __init__(self,age):
             self.total_display = 0
             self.wrapped = aClass(age)
+        #成员函数
         def display(self):
             self.total_display += 1
             print ("total display" ,self.total_display)
             self.wrapped.display()
+    #返回这个新的类
     return newClass
 
+#使用装饰器
 @decorator
+#定义类：
 class Bird:
     def __init__(self,age):
         self.age = age
