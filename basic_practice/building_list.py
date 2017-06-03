20c67954199ea1aa6086282b9df7306848f9c030
wangdejun <1074201799@qq.com>
2017-06-03 11:55:29 +0800

    new techniques with python list and python classes

---
+++ basic_practice/building_list.py
@@ -0,0 +1,1 @@
+evens_to_50 = [(i,i+1) for i in range(51) if i % 2 == 0]
---
+++ basic_practice/class_override.py
@@ -0,0 +1,15 @@
+class Employee(object):
+    """Models real-life employees!"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+# define a class inherit from class Employee 
+# override the calculate_wage() method with a new calculate method specially to PartTimeEmplyee
+class PartTimeEmployee(Employee):
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 12.00
